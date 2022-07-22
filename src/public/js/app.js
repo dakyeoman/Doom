b0bd5d93938f89b21bc1f003e386f5506d1e44e4
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2022-07-22 13:03:48 +0900

    update

---
+++ src/public/js/app.js
@@ -0,0 +1,40 @@
+
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nameForm = document.querySelector("#nickname");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function handleOpen() {
+    console.log("Connected to Server ✅");
+}
+socket.addEventListener("open", handleOpen);
+
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+});
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send({
+        type:"nickname", 
+        payload:input.value;
+    }); dfs 
+};
+
+messageForm.addEventListener("submit", handleSubmit);
+nameForm.addEventListener("submit", handleNickSubmit);
