b3b532dda00d2e90c116d54682f57e277d50b1c0
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2022-07-23 10:15:50 +0900

    update

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
@@ -24,21 +25,20 @@ wss.on("connection", (socket) => {
     sockets.push(socket);
     console.log("Connected to Browser ✅");
     socket.on("close", onSocketClose);
-
     socket.on("message", (message) => {
-        message = message.toString('utf-8')
+        JSON.parse
         sockets.forEach((aSocket) => aSocket.send(message));
     });
 });
 
-server.listen(4250, handleListen);
+server.listen(3000, handleListen);
 
 {
-    type:"message", 
-    payload:"hello everyone!"
+    type:"message";
+    payload:"hello everyone!";
 }
 
 {
-    type:"nickname"
-    payload:"nico"
+    type:"nick";
+    payload:"nico";
 }
